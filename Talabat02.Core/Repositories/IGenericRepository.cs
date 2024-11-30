bbb93c3e1e504b216ac71740a67ec3d29cf46bd2
Mohamed Madkour <Mohamed.Madkour2002@gmail.com>
2024-11-30 03:18:41 +0200

    Generic Repository

---
+++ Talabat02.Core/Repositories/IGenericRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Talabat02.Core.Entities;
+
+namespace Talabat02.Core.Repositories
+{
+	public interface IGenericRepository<T> where T : BaseEntity
+	{
+		Task<IEnumerable<T>> GetAllAsync();
+		Task<T> GetByIdAsync(int id);
+	}
+}
